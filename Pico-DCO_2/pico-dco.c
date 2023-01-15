c5e4ed91099cf837aafc6127c9cbfafc6e768fd5
KarenColumbo <116293822+KarenColumbo@users.noreply.github.com>
2023-01-15 17:54:59 +0100

    Update pico-dco.c

--- Pico-DCO_2/pico-dco.c
+++ Pico-DCO_2/pico-dco.c
@@ -15,26 +15,26 @@
 #define NUM_VOICES 8
 #define MIDI_CHANNEL 1
 //#define USE_ADC_STACK_VOICES // gpio 28 (adc 2)
-//#define USE_ADC_DETUNE       // gpio 27 (adc 1)
-//#define USE_ADC_FM           // gpio 26 (adc 0)
+#define USE_ADC_DETUNE       // gpio 27 (adc 1)
+#define USE_ADC_FM           // gpio 26 (adc 0)
 
 uint STACK_VOICES = 0;
-//float DETUNE = 0.0f, LAST_DETUNE = 0.0f;
-//float FM_VALUE = 0.0f;  
-//float LAST_FM = 0.0f;
+float DETUNE = 0.0f, LAST_DETUNE = 0.0f;
+float FM_VALUE = 0.0f;  
+float LAST_FM = 0.0f;
 
 // Scale factor for FM. Controls how intense the effect is at maximum input voltage.
 // Units: Hertz.
-//float FM_INTENSITY = 5.0f;          
+float FM_INTENSITY = 5.0f;          
 
-//const float BASE_NOTE = 440.0f;
+const float BASE_NOTE = 440.0f;
 //const uint8_t RESET_PINS[NUM_VOICES] = {13, 8, 12, 9, 11, 10, 7, 6};
-//const uint8_t RANGE_PINS[NUM_VOICES] = {16, 19, 15, 18, 14, 17, 5, 4};
+const uint8_t CV_PINS[NUM_VOICES] = {16, 19, 15, 18, 14, 17, 13, 12};
 const uint8_t GATE_PINS[NUM_VOICES] = {2, 3, 4, 5, 6, 7, 8, 9};
-//const uint8_t VOICE_TO_PIO[NUM_VOICES] = {0, 0, 0, 0, 1, 1, 1, 1};
-//const uint8_t VOICE_TO_SM[NUM_VOICES] = {0, 1, 2, 3, 0, 1, 2, 3};
-//const uint16_t DIV_COUNTER = 1250;
-//uint8_t RANGE_PWM_SLICES[NUM_VOICES];
+const uint8_t VOICE_TO_PIO[NUM_VOICES] = {0, 0, 0, 0, 1, 1, 1, 1};
+const uint8_t VOICE_TO_SM[NUM_VOICES] = {0, 1, 2, 3, 0, 1, 2, 3};
+const uint16_t DIV_COUNTER = 1250;
+uint8_t CV_PWM_SLICES[NUM_VOICES];
 
 uint32_t VOICES[NUM_VOICES];
 uint8_t VOICE_NOTES[NUM_VOICES];
@@ -44,14 +44,14 @@ uint8_t NEXT_VOICE = 0;
 uint32_t LED_BLINK_START = 0;
 PIO pio[2] = {pio0, pio1};
 uint8_t midi_serial_status = 0;
-//uint16_t midi_pitch_bend = 0x2000, last_midi_pitch_bend = 0x2000;
-//bool portamento = false;
-//uint8_t portamento_time = 0, portamento_start = 0, portamento_stop = 0;
-//float portamento_cur_freq = 0.0f;
+uint16_t midi_pitch_bend = 0x2000, last_midi_pitch_bend = 0x2000;
+bool portamento = false;
+uint8_t portamento_time = 0, portamento_start = 0, portamento_stop = 0;
+float portamento_cur_freq = 0.0f;
 
-//void init_sm(PIO pio, uint sm, uint offset, uint pin);
-//void set_frequency(PIO pio, uint sm, float freq);
-//float get_freq_from_midi_note(uint8_t note);
+void init_sm(PIO pio, uint sm, uint offset, uint pin);
+void set_frequency(PIO pio, uint sm, float freq);
+float get_freq_from_midi_note(uint8_t note);
 void led_blinking_task();
 uint8_t get_free_voice();
 void usb_midi_task();
@@ -59,7 +59,7 @@ void serial_midi_task();
 void note_on(uint8_t note, uint8_t velocity);
 void note_off(uint8_t note);
 void voice_task();
-//void adc_task();
+void adc_task();
 long map(long x, long in_min, long in_max, long out_min, long out_max);
 
 int main() {
@@ -79,19 +79,19 @@ int main() {
 
     // pwm init
     for (int i=0; i<NUM_VOICES; i++) {
-    //    gpio_set_function(RANGE_PINS[i], GPIO_FUNC_PWM);
-    //    RANGE_PWM_SLICES[i] = pwm_gpio_to_slice_num(RANGE_PINS[i]);
-    //    pwm_set_wrap(RANGE_PWM_SLICES[i], DIV_COUNTER);
-    //    pwm_set_enabled(RANGE_PWM_SLICES[i], true);
+        gpio_set_function(CV_PINS[i], GPIO_FUNC_PWM);
+        CV_PWM_SLICES[i] = pwm_gpio_to_slice_num(CV_PINS[i]);
+        pwm_set_wrap(CV_PWM_SLICES[i], DIV_COUNTER);
+        pwm_set_enabled(CV_PWM_SLICES[i], true);
     }
 
     // pio init
-    //uint offset[2];
-    //offset[0] = pio_add_program(pio[0], &frequency_program);
-    //offset[1] = pio_add_program(pio[1], &frequency_program);
-    //for (int i=0; i<NUM_VOICES; i++) {
-    //    init_sm(pio[VOICE_TO_PIO[i]], VOICE_TO_SM[i], offset[VOICE_TO_PIO[i]], RESET_PINS[i]);
-    //}
+    uint offset[2];
+    offset[0] = pio_add_program(pio[0], &frequency_program);
+    offset[1] = pio_add_program(pio[1], &frequency_program);
+    for (int i=0; i<NUM_VOICES; i++) {
+        init_sm(pio[VOICE_TO_PIO[i]], VOICE_TO_SM[i], offset[VOICE_TO_PIO[i]]);
+    }
 
     // gate gpio init
     for (int i=0; i<NUM_VOICES; i++) {
@@ -100,22 +100,22 @@ int main() {
     }
 
     // adc init
-    //#if defined(USE_ADC_STACK_VOICES) || defined(USE_ADC_DETUNE) || defined(USE_ADC_FM) 
-    //adc_init();
-    //#ifdef USE_ADC_DETUNE
-    //adc_gpio_init(27);
-    //#endif
+    #if defined(USE_ADC_STACK_VOICES) || defined(USE_ADC_DETUNE) || defined(USE_ADC_FM) 
+    adc_init();
+    #ifdef USE_ADC_DETUNE
+    adc_gpio_init(27);
+    #endif
     #ifdef USE_ADC_STACK_VOICES
     adc_gpio_init(28);
     #endif
-    //#ifdef USE_ADC_FM
-    //adc_gpio_init(26);
-    //#endif
-    //#endif
+    #ifdef USE_ADC_FM
+    adc_gpio_init(26);
+    #endif
+    #endif
 
     // init voices
     for (int i=0; i<NUM_VOICES; i++) {
-    //    VOICES[i] = 0;
+        VOICES[i] = 0;
         VOICE_GATE[i] = 0;
     }
 
@@ -124,46 +124,46 @@ int main() {
         usb_midi_task();
         serial_midi_task();
         voice_task();
-        //#if defined(USE_ADC_STACK_VOICES) || defined(USE_ADC_DETUNE) || defined(USE_ADC_FM)
-        //adc_task();
-        //#endif
+        #if defined(USE_ADC_DETUNE) || defined(USE_ADC_FM)
+        adc_task();
+        #endif
         led_blinking_task();
     }
 }
 
-//void init_sm(PIO pio, uint sm, uint offset, uint pin) {
-//    init_sm_pin(pio, sm, offset, pin);
-//    pio_sm_set_enabled(pio, sm, true);
-//}
+void init_sm(PIO pio, uint sm, uint offset, uint pin) {
+    init_sm_pin(pio, sm, offset, pin);
+    pio_sm_set_enabled(pio, sm, true);
+}
 
-//void set_frequency(PIO pio, uint sm, float freq) {
-//    uint32_t clk_div = clock_get_hz(clk_sys) / 2 / freq;
-//    if (freq == 0) clk_div = 0;
-//    pio_sm_put(pio, sm, clk_div);
-//    pio_sm_exec(pio, sm, pio_encode_pull(false, false));
-//    pio_sm_exec(pio, sm, pio_encode_out(pio_y, 32));
-//}
+void set_frequency(PIO pio, uint sm, float freq) {
+    uint32_t clk_div = clock_get_hz(clk_sys) / 2 / freq;
+    if (freq == 0) clk_div = 0;
+    pio_sm_put(pio, sm, clk_div);
+    pio_sm_exec(pio, sm, pio_encode_pull(false, false));
+    pio_sm_exec(pio, sm, pio_encode_out(pio_y, 32));
+}
 
-//float get_freq_from_midi_note(uint8_t note) {
-//    if (portamento && portamento_start != 0 && portamento_stop != 0) {
-//        float freq1 = pow(2, (portamento_start-69)/12.0f) * BASE_NOTE;
-//        float freq2 = pow(2, (portamento_stop-69)/12.0f) * BASE_NOTE;
-//        if (portamento_cur_freq == 0) {
-//            portamento_cur_freq = freq1;
-//        } else {
-//            if (freq1 < freq2) {
-//                portamento_cur_freq += 1.0f/(portamento_time+1);
-//                if (portamento_cur_freq > freq2) portamento_cur_freq = freq2;
-//            } else {
-//                portamento_cur_freq -= 1.0f/(portamento_time+1);
-//                if (portamento_cur_freq < freq2) portamento_cur_freq = freq2;
-//            }
-//        }
-//        return portamento_cur_freq;
-//    }
-//
-//    return pow(2, (note-69)/12.0f) * BASE_NOTE;
-//}
+float get_freq_from_midi_note(uint8_t note) {
+    if (portamento && portamento_start != 0 && portamento_stop != 0) {
+        float freq1 = pow(2, (portamento_start-69)/12.0f) * BASE_NOTE;
+        float freq2 = pow(2, (portamento_stop-69)/12.0f) * BASE_NOTE;
+        if (portamento_cur_freq == 0) {
+            portamento_cur_freq = freq1;
+        } else {
+            if (freq1 < freq2) {
+                portamento_cur_freq += 1.0f/(portamento_time+1);
+                if (portamento_cur_freq > freq2) portamento_cur_freq = freq2;
+            } else {
+                portamento_cur_freq -= 1.0f/(portamento_time+1);
+                if (portamento_cur_freq < freq2) portamento_cur_freq = freq2;
+            }
+        }
+        return portamento_cur_freq;
+    }
+
+    return pow(2, (note-69)/12.0f) * BASE_NOTE;
+}
 
 void usb_midi_task() {
     if (tud_midi_available() < 4) return;
@@ -186,17 +186,17 @@ void usb_midi_task() {
             note_off(buff[2]);
         }
 
-        //if (buff[1] == (0xE0 | (MIDI_CHANNEL-1))) {
-        //    midi_pitch_bend = buff[2] | (buff[3]<<7);
-        //}
+        if (buff[1] == (0xE0 | (MIDI_CHANNEL-1))) {
+            midi_pitch_bend = buff[2] | (buff[3]<<7);
+        }
 
         if (midi_serial_status == (0xB0 | (MIDI_CHANNEL-1))) {
-        //    if (buff[2] == 5) { // portamento time
-        //        portamento_time = buff[3];
-        //    }
-        //    if (buff[2] == 65) { // portamento on/off
-        //        portamento = buff[3] > 63;
-        //    }
+            if (buff[2] == 5) { // portamento time
+                portamento_time = buff[3];
+            }
+            if (buff[2] == 65) { // portamento on/off
+                portamento = buff[3] > 63;
+            }
         }
     }
 }
@@ -247,16 +247,16 @@ void serial_midi_task() {
     }
 
     if (midi_serial_status == (0xE0 | (MIDI_CHANNEL-1))) {
-    //    midi_pitch_bend = lsb | (msb<<7);
+        midi_pitch_bend = lsb | (msb<<7);
     }
 
     if (midi_serial_status == (0xB0 | (MIDI_CHANNEL-1))) {
-    //    if (lsb == 5) { // portamento time
-    //        portamento_time = msb;
-    //    }
-    //    if (lsb == 65) { // portamento on/off
-    //         portamento = msb > 63;
-    //    }
+        if (lsb == 5) { // portamento time
+            portamento_time = msb;
+        }
+        if (lsb == 65) { // portamento on/off
+             portamento = msb > 63;
+        }
     }
 }
 
@@ -270,25 +270,25 @@ void note_on(uint8_t note, uint8_t velocity) {
         uint8_t voice_num = get_free_voice();
 
         VOICES[voice_num] = board_millis();
-        //VOICE_NOTES[voice_num] = note;
+        VOICE_NOTES[voice_num] = note;
         VOICE_GATE[voice_num] = 1;
 
-        //float freq = get_freq_from_midi_note(note) * (1 + (pow(-1, i) * DETUNE));
-        //set_frequency(pio[VOICE_TO_PIO[voice_num]], VOICE_TO_SM[voice_num], freq);
-        // amplitude adjustment
-        //pwm_set_chan_level(RANGE_PWM_SLICES[voice_num], pwm_gpio_to_channel(RANGE_PINS[voice_num]), (int)(DIV_COUNTER*(freq*0.00025f-1/(100*freq))));
-        // gate on
-        //gpio_put(GATE_PINS[voice_num], 1);
+        float freq = get_freq_from_midi_note(note) * (1 + (pow(-1, i) * DETUNE));
+        set_frequency(pio[VOICE_TO_PIO[voice_num]], VOICE_TO_SM[voice_num], freq);
+         amplitude adjustment
+        pwm_set_chan_level(CV_PWM_SLICES[voice_num], pwm_gpio_to_channel(CV_PINS[voice_num]), (int)(DIV_COUNTER*(freq*0.00025f-1/(100*freq))));
+         gate on
+        gpio_put(GATE_PINS[voice_num], 1);
     }
-    //if (portamento) {
-    //    if (portamento_start == 0) {
-    //        portamento_start = note;
-    //        portamento_cur_freq = 0.0f;
-    //    } else {
-    //        portamento_stop = note;
-    //    }
-    //}
-    //last_midi_pitch_bend = 0;
+    if (portamento) {
+        if (portamento_start == 0) {
+            portamento_start = note;
+            portamento_cur_freq = 0.0f;
+        } else {
+            portamento_stop = note;
+        }
+    }
+    last_midi_pitch_bend = 0;
 }
 
 void note_off(uint8_t note) {
@@ -302,16 +302,16 @@ void note_off(uint8_t note) {
             VOICE_GATE[i] = 0;
         }
     }
-    //if (portamento_stop == note) {
-    //    portamento_start = portamento_stop;
-    //    portamento_stop = 0;
-    //    portamento_cur_freq = 0.0f;
-    //}
+    if (portamento_stop == note) {
+        portamento_start = portamento_stop;
+        portamento_stop = 0;
+        portamento_cur_freq = 0.0f;
+    }
     /*
-    //if (portamento_start == note) {
-    //    portamento_stop = 0;
-    //    portamento_cur_freq = 0.0f;
-    //}
+    if (portamento_start == note) {
+        portamento_stop = 0;
+        portamento_cur_freq = 0.0f;
+    }
     */
 }
 
@@ -338,53 +338,53 @@ uint8_t get_free_voice() {
 }
 
 void voice_task() {
-//    if (midi_pitch_bend != last_midi_pitch_bend || DETUNE != LAST_DETUNE || portamento || FM_VALUE != LAST_FM) {
-//
-//        last_midi_pitch_bend = midi_pitch_bend;
-//        LAST_DETUNE = DETUNE;
-//        LAST_FM = FM_VALUE;
-//
-//        for (int i=0; i<NUM_VOICES; i++) {
-//
-//            float freq = get_freq_from_midi_note(VOICE_NOTES[i]) * (1 + (pow(-1, i) * DETUNE));
-//
-//            freq += FM_VALUE * FM_INTENSITY; // Add linear frequency modulation
-//            
-//            freq = freq-(freq*((0x2000-midi_pitch_bend)/67000.0f));
-//            set_frequency(pio[VOICE_TO_PIO[i]], VOICE_TO_SM[i], freq);
-//            pwm_set_chan_level(RANGE_PWM_SLICES[i], pwm_gpio_to_channel(RANGE_PINS[i]), (int)(DIV_COUNTER*(freq*0.00025f-1/(100*freq))));
-//        }
-//    }
+    if (midi_pitch_bend != last_midi_pitch_bend || DETUNE != LAST_DETUNE || portamento || FM_VALUE != LAST_FM) {
+
+        last_midi_pitch_bend = midi_pitch_bend;
+        LAST_DETUNE = DETUNE;
+        LAST_FM = FM_VALUE;
+
+        for (int i=0; i<NUM_VOICES; i++) {
+
+            float freq = get_freq_from_midi_note(VOICE_NOTES[i]) * (1 + (pow(-1, i) * DETUNE));
+
+            freq += FM_VALUE * FM_INTENSITY; // Add linear frequency modulation
+           
+            freq = freq-(freq*((0x2000-midi_pitch_bend)/67000.0f));
+            set_frequency(pio[VOICE_TO_PIO[i]], VOICE_TO_SM[i], freq);
+            pwm_set_chan_level(CV_PWM_SLICES[i], pwm_gpio_to_channel(CV_PINS[i]), (int)(DIV_COUNTER*(freq*0.00025f-1/(100*freq))));
+        }
+    }
 }
 
 void adc_task() {
-//    uint16_t raw;
-//
-//    #ifdef USE_ADC_DETUNE
-//    adc_select_input(1);
-//    raw = adc_read();
-//    DETUNE = map(raw, 0, 4095, 0, 50)/1000.0f;
-//    #endif
-//
-//    #ifdef USE_ADC_STACK_VOICES
-//    adc_select_input(2);
-//    raw = adc_read();
-//    STACK_VOICES = map(raw, 0, 4000, 1, NUM_VOICES);
-//    #endif
-//
-//    #ifdef USE_ADC_FM
-//
-//    adc_select_input(0);
-//    raw = adc_read();
-//
+    uint16_t raw;
+
+    #ifdef USE_ADC_DETUNE
+    adc_select_input(1);
+    raw = adc_read();
+    DETUNE = map(raw, 0, 4095, 0, 50)/1000.0f;
+    #endif
+
+    #ifdef USE_ADC_STACK_VOICES
+    adc_select_input(2);
+    raw = adc_read();
+    STACK_VOICES = map(raw, 0, 4000, 1, NUM_VOICES);
+    #endif
+
+    #ifdef USE_ADC_FM
+
+    adc_select_input(0);
+    raw = adc_read();
+
     // This input assumes a centre FM value of 2^11 = 2048, as the 
     // ADCs are unsigned. This can be overcome in hardware with a fixed 
     // voltage offset. The range of the Pico's ADCs is 3.3V, so a fixed 
     // offset of 1.65V is needed.
-//    int signed_raw = raw - (1 << 11);
-//    FM_VALUE = (float) signed_raw / (float) (1 << 11);
-//    #endif
-//
+    int signed_raw = raw - (1 << 11);
+    FM_VALUE = (float) signed_raw / (float) (1 << 11);
+    #endif
+
 }
 
 long map(long x, long in_min, long in_max, long out_min, long out_max) {
